4abf020c991b452307d0ef351769897f4b810498
Felipe Garcia <felipe901230@gmail.com>
2016-11-22 15:00:43 -0500

    agregamos file delete_proveedor para eliminar

---
+++ coordinador/controllers/delete_proveedor.php
@@ -0,0 +1,15 @@
+<?php 
+
+	include "../../conexion.php";
+
+	$id = $_GET['id'];
+
+
+					$sql1=pg_query($conexion,"DELETE FROM facturacionhts.proveedor
+  		WHERE id_proveedor=$id ") ;
+ 
+ 					echo ' <script language="javascript">alert("Datos actualizados con exito.");</script> ';
+ 					echo "<script>location.href='../proveedor.php'</script>";
+
+
+?>
